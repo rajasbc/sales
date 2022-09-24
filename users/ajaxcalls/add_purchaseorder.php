97880ee1c5da03c6e4096fa4c50796b161ff87bc
balunig051 <balasubramani@nigsoft.com>
2022-09-24 18:35:04 +0530

    error check

--- users/ajaxcalls/add_purchaseorder.php
+++ users/ajaxcalls/add_purchaseorder.php
@@ -1,9 +1,8 @@
 <?php
 include '../../includes/config.php';
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
 $obj = new Purchaseorder();
 $result = $obj->addbill_details();
 echo json_encode($result);
 ?>
-
 
