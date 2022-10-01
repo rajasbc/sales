f6ef1c7d11e4bcdae5a68c581ed20238634c81ce
Amarshanmuga <amar@nigsoft.com>
2022-10-01 11:21:04 +0530

    document added

---
+++ users/ajaxcalls/delete_documents.php
@@ -0,0 +1,14 @@
+<?php
+include '../../includes/config.php';
+// error_reporting(E_ALL);
+if ($_POST['type']=='sales') {
+$obj = new Sales();
+$result = $obj->delete_document($_POST['id']);
+}else if ($_POST['type']=='purchase') {
+$obj = new Purchase();
+$result = $obj->delete_document($_POST['id']);
+}
+
+echo json_encode($result);
+?>
+
