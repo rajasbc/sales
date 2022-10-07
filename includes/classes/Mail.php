926c989418bd7877662c5593b136f08e5278ed52
balunig051 <balasubramani@nigsoft.com>
2022-10-07 17:01:41 +0530

    mail check

--- includes/classes/Mail.php
+++ includes/classes/Mail.php
@@ -1,11 +1,12 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 class Mail extends Dbconnection {
 	var $mail;
 	var $db;
+
+	// use PHPMailer\PHPMailer\PHPMailer;
+	// use PHPMailer\PHPMailer\Exception;
+
 
 	function __construct() {
 		parent::__construct();
@@ -14,18 +15,20 @@ class Mail extends Dbconnection {
 
 	public function sendEmail(){
 
-		$mail = new PHPMailer(true);
+		$mail = new PHPMailer\PHPMailer\PHPMailer(true);
 
 		$mail->From = "adminpo@2crsi.com";
 		$mail->FromName = "Admin";
 
 
-		$mail->addAddress($users['email'], $users['name']);
+		$mail->addAddress($this->db->getpost('email_id'), $this->db->getpost('username'));
 
 		$mail->isHTML(true);
 
 		$mail->Subject = "Registered as PO Tracking System - 2crsi";
-		$mail->Body = "Hi, You are registered as 2crsi PO Tracking System, Your login details below., <br /><br /><table><tr><td>User Name : ".$users['username']."</td></tr><tr><td>User Name : ".$originalpassword."</td></tr></table><br />Thank You,";
+		$mail->Body = "Hi, You are registered as 2crsi PO Tracking System, Your login details below., <br /><br /><table><tr><td>User Name : ".$this->db->getpost('username')."</td></tr><tr><td>User Name : ".$this->db->getpost('password')."</td></tr></table><br />Thank You,";
+
+		// echo $mail;
 
 		try {
 		    $mail->send();
--- users/ajaxcalls/addUser.php
+++ users/ajaxcalls/addUser.php
@@ -1,6 +1,9 @@
 <?php
 include '../../includes/config.php';
 
+
+// error_reporting(E_ALL);
+
 $obj = new Admin();
 $result =  $obj->add_users();
 
